d3b373d3b5116bb219b0bdcdc3008cf1cb18dd46
AbhishekMandapmalvi <abhishekrmandapmalvi@gmail.com>
2025-09-07 21:06:46 -0400

    updated 122.[y

--- 122 Best Time to Buy and Sell Stock II/122.py
+++ 122 Best Time to Buy and Sell Stock II/122.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans=0
+        profit=0
 
-        for f in range(1,len(prices)):
-            if prices[f]>prices[f-1]:
-                ans+=prices[f]-prices[f-1]
-            
-        return ans
+        for price in range(1,len(prices)):
+            if prices[price]-prices[price-1]>0:
+                profit+=prices[price]-prices[price-1]
+
+        return profit
