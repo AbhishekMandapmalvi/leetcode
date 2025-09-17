8fad4101042b0cd0d286a8098a8ac3af6f4b0fb9
AbhishekMandapmalvi <abhishekrmandapmalvi@gmail.com>
2025-09-17 18:34:25 -0400

    updated 56

--- 56 Merge-intervals/56.py
+++ 56 Merge-intervals/56.py
@@ -4,13 +4,13 @@ class Solution:
         result=[intervals[0]]
 
         for i in range(1,len(intervals)):
-            print(result,intervals)
-            if intervals[i][0]<=intervals[i-1][1] and intervals[i][1]>intervals[i-1][1]:
-                result.pop()
-                result.append([intervals[i-1][0],intervals[i][1]])
-            elif intervals[i][0]<=intervals[i-1][1] and intervals[i][1]<=intervals[i-1][1]:
-                result.pop()
-                result.append([intervals[i-1][0],intervals[i-1][1]])
+            
+            if intervals[i][0]<=result[-1][1] and intervals[i][1]>result[-1][1]:
+                result[-1][1]=intervals[i][1]
+            
+            elif intervals[i][0]<=result[-1][1] and intervals[i][1]<=result[-1][1]:
+                pass
+            
             else:
                 result.append(intervals[i])
         
